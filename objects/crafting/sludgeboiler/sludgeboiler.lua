f5bd1994a74748a2dfe83d6f9b504f1d8a5608d0
IbraheemRodrigues <ibraheemrodrigues@gmail.com>
2018-02-15 22:04:28 +0000

    Planning changes (Issue #6) Also not UNUSED reminder added to left & right mirror, and inverse lens after planning

--- objects/crafting/sludgeboiler/sludgeboiler.lua
+++ objects/crafting/sludgeboiler/sludgeboiler.lua
@@ -1,9 +1,10 @@
 function update(dt)
 	for i=0, 15, 1 do
 	  item = world.containerItemAt(entity.id(), i)
-	  if (item and item.name ~= "otmgreymatter") then
-		world.containerConsumeAt(entity.id(), i, 1)
-		world.containerAddItems(entity.id(), { name= "otmgreymatter", count = 1})
+		if (item and item.name ~= "otmgreymatter" and item.name ~= "otmdarkmatter" and item.name ~= "otmwhitematter") then
+			local type = math.random(0, 1000) == 0 and 'otmgreymatter' or 'otmdarkmatter'
+			world.containerConsumeAt(entity.id(), i, 1)
+			world.containerAddItems(entity.id(), { name=type, count=1})
 	  end
 	end
 end
