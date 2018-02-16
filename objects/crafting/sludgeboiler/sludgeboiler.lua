a2be19a3522b5ecbdce4522eefe91f539d7c5731
IbraheemRodrigues <ibraheemrodrigues@gmail.com>
2018-02-16 11:40:54 +0000

    - Added matter and mineral crafting mrecipes (a1.3) - More tweaks following issue #6 - Fixed sludge boiler issue(gave darkmatter 99‰ of time instead of 1‰)

--- objects/crafting/sludgeboiler/sludgeboiler.lua
+++ objects/crafting/sludgeboiler/sludgeboiler.lua
@@ -2,7 +2,7 @@ function update(dt)
 	for i=0, 15, 1 do
 	  item = world.containerItemAt(entity.id(), i)
 		if (item and item.name ~= "otmgreymatter" and item.name ~= "otmdarkmatter" and item.name ~= "otmwhitematter") then
-			local type = math.random(0, 1000) == 0 and 'otmgreymatter' or 'otmdarkmatter'
+			local type = math.random(0, 1000) == 0 and 'otmdarkmatter' or 'otmgreymatter'
 			world.containerConsumeAt(entity.id(), i, 1)
 			world.containerAddItems(entity.id(), { name=type, count=1})
 	  end
